c48ee2474ac05587e280ae4800ff98ca7656cc18
George <georgehayfield96@gmail.com>
2021-11-30 18:15:02 +0000

    Edited the db files in the server folder

--- REAL_Building_Systems/server/config/db.js
+++ REAL_Building_Systems/server/config/db.js
@@ -23,31 +23,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
-/*const Express = require("express");
-const BodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
-
-const CONNECTION_URL = "mongodb+srv://FederalTroll1:IndicatePassedStop31102000@cluster0.ns7it.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const DATABASE_NAME = "myFirstDatabase";
-
-var app = Express();
-
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
-
-var database, collection;
-
-app.listen(3000, () => {
-    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        database = client.db(DATABASE_NAME);
-        collection = database.collection("people");
-        console.log("Connected to `" + DATABASE_NAME + "`!");
-    });
-});
-*/

9976ec4113d64a505ca16a2a9e651f839af62ed5
George <georgehayfield96@gmail.com>
2021-12-01 15:50:15 +0000

    Added changes to the book model and also web vitals

--- REAL_Building_Systems/server/routes/api/books.js
+++ REAL_Building_Systems/server/routes/api/books.js
@@ -2,22 +2,25 @@
 /* eslint-env es6 */
 /* eslint-disable */
 
-const express = require('express');
-const router = express.Router();
+
+//CAN DELETE THIS FILE
+//CHECK AT END
+
+
+const expressConnection = require('express');
+const routerConnection = expressConnection.Router();
 
 // Load Book model that has the connection format
 //required in the database
 const Book = require('../../models/Book');
 
-// @route GET api/books/test
-// @description tests books route
-// @access Public
-router.get('/test', (req, res) => res.send('book route testing!'));
+//GET api/books/test to test the books route
+routerConnection().get('/test', (req, res) => res.send('Testing Book Route!'));
 
 // @route GET api/books
 // @description Get all books
 // @access Public
-router.get('/', (req, res) => {
+routerConnection().get('/', (req, res) => {
   Book.find()
     .then(books => res.json(books))
     .catch(err => res.status(404).json({ nobooksfound: 'No Books found' }));
@@ -26,7 +29,7 @@ router.get('/', (req, res) => {
 // @route GET api/books/:id
 // @description Get single book by id
 // @access Public
-router.get('/:id', (req, res) => {
+routerConnection().get('/:id', (req, res) => {
   Book.findById(req.params.id)
     .then(book => res.json(book))
     .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
@@ -35,7 +38,7 @@ router.get('/:id', (req, res) => {
 // @route GET api/books
 // @description add/save book
 // @access Public
-router.post('/', (req, res) => {
+routerConnection().post('/', (req, res) => {
   Book.create(req.body)
     .then(book => res.json({ msg: 'Book added successfully' }))
     .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
@@ -44,7 +47,7 @@ router.post('/', (req, res) => {
 // @route GET api/books/:id
 // @description Update book
 // @access Public
-router.put('/:id', (req, res) => {
+routerConnection().put('/:id', (req, res) => {
   Book.findByIdAndUpdate(req.params.id, req.body)
     .then(book => res.json({ msg: 'Updated successfully' }))
     .catch(err =>
@@ -55,10 +58,10 @@ router.put('/:id', (req, res) => {
 // @route GET api/books/:id
 // @description Delete book by id
 // @access Public
-router.delete('/:id', (req, res) => {
+routerConnection().delete('/:id', (req, res) => {
   Book.findByIdAndRemove(req.params.id, req.body)
     .then(book => res.json({ mgs: 'Book entry deleted successfully' }))
     .catch(err => res.status(404).json({ error: 'No such a book' }));
 });
 
-module.exports = router;
+module.exports = routerConnection();
